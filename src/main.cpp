867f3624796a63618bc74676a1346f53f47d4b1b
Juan Falquez <juan.falquez@colorado.edu>
2015-04-26 16:13:57 -0600

    Fixed to new CRTP model.

--- src/main.cpp
+++ src/main.cpp
@@ -37,95 +37,26 @@ Street, Fifth Floor, Boston, MA 02110-1301, USA
 using namespace std;
 
 
-
-inline Sophus::SE3d T_rlFromCamModelRDF(const calibu::CameraModelAndTransform& lcmod, const calibu::CameraModelAndTransform& rcmod, const Eigen::Matrix3d& targetRDF)
+inline Sophus::SE3d T_rlFromCamModelRDF(
+    const std::shared_ptr<calibu::CameraInterface<double>> lcmod,
+    const std::shared_ptr<calibu::CameraInterface<double>> rcmod,
+    const Eigen::Matrix3d& targetRDF)
 {
-  // Transformation matrix to adjust to target RDF
-  Eigen::Matrix4d Tadj[2] = {Eigen::Matrix4d::Identity(),Eigen::Matrix4d::Identity()};
-  Tadj[0].block<3,3>(0,0) = targetRDF.transpose() * lcmod.camera.RDF();
-  Tadj[1].block<3,3>(0,0) = targetRDF.transpose() * rcmod.camera.RDF();
+    // Transformation matrix to adjust to target RDF
+    Eigen::Matrix4d Tadj[2] = {Eigen::Matrix4d::Identity(),Eigen::Matrix4d::Identity()};
+    Tadj[0].block<3,3>(0,0) = targetRDF.transpose() * lcmod->RDF();
+    Tadj[1].block<3,3>(0,0) = targetRDF.transpose() * rcmod->RDF();
 
-  // Computer Poses in our adjust coordinate system
-  const Eigen::Matrix4d T_lw_ = Tadj[0] * lcmod.T_wc.matrix().inverse();
-  const Eigen::Matrix4d T_rw_ = Tadj[1] * rcmod.T_wc.matrix().inverse();
+    // Computer Poses in our adjust coordinate system
+    const Eigen::Matrix4d T_lw_ = Tadj[0] * lcmod->Pose().matrix().inverse();
+    const Eigen::Matrix4d T_rw_ = Tadj[1] * rcmod->Pose().matrix().inverse();
 
-  // Computer transformation to right camera frame from left
-  const Eigen::Matrix4d T_rl = T_rw_ * T_lw_.inverse();
+    // Computer transformation to right camera frame from left
+    const Eigen::Matrix4d T_rl = T_rw_ * T_lw_.inverse();
 
-  return Sophus::SE3d(T_rl.block<3,3>(0,0), T_rl.block<3,1>(0,3) );
+    return Sophus::SE3d(T_rl.block<3,3>(0,0), T_rl.block<3,1>(0,3) );
 }
 
-
-// compute disparities of pgm image input pair file_1, file_2
-void process (const char* file_1,const char* file_2) {
-
-
-  // load images
-  image<uchar> *I1,*I2;
-  I1 = loadPGM(file_1);
-  I2 = loadPGM(file_2);
-
-  // check for correct size
-  if (I1->width()<=0 || I1->height() <=0 || I2->width()<=0 || I2->height() <=0 ||
-      I1->width()!=I2->width() || I1->height()!=I2->height()) {
-    cout << "ERROR: Images must be of same size, but" << endl;
-    cout << "       I1: " << I1->width() <<  " x " << I1->height() <<
-            ", I2: " << I2->width() <<  " x " << I2->height() << endl;
-    delete I1;
-    delete I2;
-    return;
-  }
-
-  // get image width and height
-  int32_t width  = I1->width();
-  int32_t height = I1->height();
-
-  // allocate memory for disparity images
-  const int32_t dims[3] = {width,height,width}; // bytes per line = width
-  float* D1_data = (float*)malloc(width*height*sizeof(float));
-  float* D2_data = (float*)malloc(width*height*sizeof(float));
-
-  // process
-  Elas::parameters param;
-  param.postprocess_only_left = false;
-  Elas elas(param);
-  elas.process(I1->data,I2->data,D1_data,D2_data,dims);
-
-  // find maximum disparity for scaling output disparity images to [0..255]
-  float disp_max = 0;
-  for (int32_t i=0; i<width*height; i++) {
-    if (D1_data[i]>disp_max) disp_max = D1_data[i];
-    if (D2_data[i]>disp_max) disp_max = D2_data[i];
-  }
-
-  // copy float to uchar
-  image<uchar> *D1 = new image<uchar>(width,height);
-  image<uchar> *D2 = new image<uchar>(width,height);
-  for (int32_t i=0; i<width*height; i++) {
-    D1->data[i] = (uint8_t)max(255.0*D1_data[i]/disp_max,0.0);
-    D2->data[i] = (uint8_t)max(255.0*D2_data[i]/disp_max,0.0);
-  }
-
-  // save disparity images
-  char output_1[1024];
-  char output_2[1024];
-  strncpy(output_1,file_1,strlen(file_1)-4);
-  strncpy(output_2,file_2,strlen(file_2)-4);
-  output_1[strlen(file_1)-4] = '\0';
-  output_2[strlen(file_2)-4] = '\0';
-  strcat(output_1,"_disp.pgm");
-  strcat(output_2,"_disp.pgm");
-  savePGM(D1,output_1);
-  savePGM(D2,output_2);
-
-  // free memory
-  delete I1;
-  delete I2;
-  delete D1;
-  delete D2;
-  free(D1_data);
-  free(D2_data);
-}
 
 int main (int argc, char** argv) {
 
@@ -144,17 +75,11 @@ int main (int argc, char** argv) {
   // Check if camera is being rectified. If so, use that camera model.
   // Otherwise, load a camera model from file.
   // Load Camera intrinsics from file.
-  calibu::CameraRig rig;
+  std::shared_ptr<calibu::Rig<double>> rig;
   hal::RectifyDriver *driver_ptr = camera.GetDriver<hal::RectifyDriver>();
   if (driver_ptr) {
     std::cerr << "Rectified driver detected. Extracting new camera model." << std::endl;
-    calibu::CameraRig driver_rig;
-    // get camera models from driver
-    calibu::CameraModelGeneric<double> cmod;
-    cmod = driver_ptr->CameraModel();
-    driver_rig.Add(cmod, Sophus::SE3Group<double>());
-    driver_rig.Add(cmod, driver_ptr->T_rl().inverse());
-    rig = driver_rig;
+    rig = driver_ptr->Rig();
   } else {
     const std::string filename = clArgs.follow("","-cmod");
     if( filename.empty() ) {
@@ -166,13 +91,13 @@ int main (int argc, char** argv) {
 
 
 
-  if (rig.cameras.size() != 2) {
+  if (rig->NumCams() != 2) {
     std::cerr << "Two camera models are required to run this program!" << std::endl;
     exit(1);
   }
 
-  Eigen::Matrix3f CamModel0 = rig.cameras[0].camera.K().cast<float>();
-  Eigen::Matrix3f CamModel1 = rig.cameras[1].camera.K().cast<float>();
+  Eigen::Matrix3f CamModel0 = rig->cameras_[0]->K().cast<float>();
+  Eigen::Matrix3f CamModel1 = rig->cameras_[1]->K().cast<float>();
 
   roo::ImageIntrinsics camMod[] = {
     {CamModel0(0,0),CamModel0(1,1),CamModel0(0,2),CamModel0(1,2)},
@@ -191,7 +116,8 @@ int main (int argc, char** argv) {
   Eigen::Matrix4d T_ro_vis = Eigen::Matrix4d::Identity();
   T_ro_vis.block<3,3>(0,0) = RDFrobot.transpose() * RDFvision;
 
-  const Sophus::SE3d T_rl = T_rlFromCamModelRDF(rig.cameras[0], rig.cameras[1], RDFvision);
+  const Sophus::SE3d T_rl = rig->cameras_[0]->Pose().inverse()
+                                * rig->cameras_[1]->Pose();
   const double baseline = T_rl.translation().norm();
 
   std::cout << "Baseline is: " << baseline << std::endl;
@@ -202,7 +128,7 @@ int main (int argc, char** argv) {
   cv::Mat hDisparity2 = cv::Mat(height, width, CV_32FC1);
   cv::Mat hDepth = cv::Mat(height, width, CV_32FC1);
 
-  std::shared_ptr<pb::ImageArray> images = pb::ImageArray::Create();
+  std::shared_ptr<hal::ImageArray> images = hal::ImageArray::Create();
 
   // ELAS image format
   image<uchar> *I1 = new image<uchar>(width, height);
